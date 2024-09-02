e2962608b62a344ee832ebcfe5de941315cab846
Andranik <tednaaa@gmail.com>
2024-09-02 19:32:19 +0400

    solution of contains_duplicate

---
+++ crates/contains_duplicate/src/lib.rs
@@ -0,0 +1,51 @@
+use std::collections::HashSet;
+
+#[must_use]
+pub fn contains_duplicate(numbers: Vec<i32>) -> bool {
+    let mut set = HashSet::new();
+
+    for number in numbers {
+        if set.contains(&number) {
+            return true;
+        }
+
+        set.insert(number);
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_duplicate() {
+        let contains = contains_duplicate(vec![1, 2, 3, 1]);
+        assert!(contains);
+    }
+
+    #[test]
+    fn many_duplicates() {
+        let contains = contains_duplicate(vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2]);
+        assert!(contains);
+    }
+
+    #[test]
+    fn no_duplicates() {
+        let contains = contains_duplicate(vec![1, 2, 3, 4]);
+        assert!(!contains);
+    }
+
+    #[test]
+    fn empty_vec() {
+        let contains = contains_duplicate(vec![]);
+        assert!(!contains);
+    }
+
+    #[test]
+    fn one_element() {
+        let contains = contains_duplicate(vec![1]);
+        assert!(!contains);
+    }
+}
